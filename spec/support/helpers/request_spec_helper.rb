36d974bf3bca7307623a770334cee8312348bb5b
Luca Tironi <luca.tironi@gmail.com>
2025-03-11 23:13:18 +0100

    Add RequestSpecHelper to facilitate login in specs

---
+++ spec/support/helpers/request_spec_helper.rb
@@ -0,0 +1,11 @@
+module RequestSpecHelper
+  def parsed_json
+    JSON.parse(response.body)
+  end
+
+  def login_user_and_get_token(email_address, password)
+    post "/sessions", params: { email_address: email_address, password: password }
+    sleep 1.seconds
+    return parsed_json["token"]
+  end
+end
